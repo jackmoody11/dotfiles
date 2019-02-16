1858142639a10f50bc05e304d074d4dd6122aee5
jackmoody11 <jackmoody@unc.edu>
2019-02-16 23:10:31 +0100

    Add jupyter setup script and settings for notebooks

---
+++ .jupyter/jupyter.sh
@@ -0,0 +1,4 @@
+# Install Jupyter extensions package
+pip3 install jupyter_contrib_nbextensions && jupyter contrib nbextension install 
+# Add autocompletion to Jupyter
+jupyter nbextension enable hinterland/hinterland 
