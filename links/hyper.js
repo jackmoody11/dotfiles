fc73d2b3794b41eb36bf0927f6a75192c2e7f866
jackmoody11 <jackmoody@unc.edu>
2019-08-09 09:19:18 -0400

    Change default font to be consistent with VS Code

--- links/hyper.js
+++ links/hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: "'SF Mono', Menlo, Monaco, 'Courier New', monospace",
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
